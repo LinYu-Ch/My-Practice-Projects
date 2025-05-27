3a8d497c182d14c8809d282d29ba2a6aa0d81146
linyu-ch <linyuenhong@gmail.com>
2025-05-27 14:42:30 -0500

    added implementation note to weighted array selector

--- Streamelements Gacha/completedModules/weightedArraySelector.js
+++ Streamelements Gacha/completedModules/weightedArraySelector.js
@@ -3,6 +3,7 @@
  * @returns {GEObject} GEObject gacha event object, contains the data to display an event
  */
 function randomGachaTier(weightedArray) {
+  // additional note in case we forget: max range does not have to be counted. since this Is a summed array, the weight of the last element will be the same size as the total weight of the array
   const maxRange = weightedArray[weightedArray.length - 1].weight;
   const randomNum = Math.random() * maxRange;
 
