86609a499e98bf894746c3e23ef580859eb53ec4
linyu-ch <linyuenhong@gmail.com>
2025-05-28 13:20:31 -0500

    defined object libraries

---
+++ Streamelements Gacha/completedModules/systemTables.js
@@ -0,0 +1,11 @@
+// global tables
+const costTable = {
+    COMMAND: 1,
+    GIFTSUB: 5,
+    DONATION: 25,
+    CHEER: 2500,
+    POINTS: 10000,
+}
+const commandDispatchTable = {
+    ROLL: (callback) => callback()
+}
